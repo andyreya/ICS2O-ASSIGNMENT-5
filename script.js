fe7551190e4e69c07d1098640a2b74c0e23cd78b
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-25 23:23:34 +0000

    website content

--- script.js
+++ script.js
@@ -6,12 +6,26 @@ let circumference = 0
 let radius = 0
 
 /* The program will wait for the calculate button to be clicked then it will activate the calculateOne function */
-document.getElementById('button-one').addEventListener('click', calculateOne)
-/* */
-function calculateOne (area) {
+document.getElementById('button-one').addEventListener('click', calculate)
+/* The */
+function calculate (area) {
   userInputOne = document.getElementById('input-one').value 
   userInputOne = parseInt(userInputOne)
   radius = userInputOne / 2
   area = Math.PI * radius ** 2
-  document.getElementById('answer-one').innerHTML = 'The area of this circle is ' + area
+  area = area.toFixed(2)
+  document.getElementById('answer-one').innerHTML = ('The area of this circle is ' + area)
+}
+
+/* */
+document.getElementById('button-two').addEventListener('click', calculate)
+
+/* */
+function calculate (circumference) {
+  userInputTwo = document.getElementById('input-two').value
+  userInputTwo = parseInt(userInputTwo)
+  radius = userInputTwo / 2
+  circumference = Math.PI * radius * 2
+  circumference = circumfernce.toFixed(2)
+  document.getElementById('answer-two').innerHTML = ('The circumference of this circle is ' + circumference)
 }
