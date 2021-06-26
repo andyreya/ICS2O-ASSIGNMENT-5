0cfd60ad83ad1d0748ceafa3738131e1f361ba94
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-26 05:08:53 +0000

    spelling

--- script2.js
+++ script2.js
@@ -6,7 +6,7 @@ let radius = 0
 // makes action occur when button is clicked
 document.getElementById('button-two').addEventListener('click', calculate)
 
-// the function changes the input into an integer and creates a value for the radius. It calculates the value of the cicumference and rounds it to two decimal places. It then displays the statement 'The circumference of this circle is ' and the cicumference afterwards
+// the function changes the input into an integer and creates a value for the radius. It calculates the value of the circumference and rounds it to two decimal places. It then displays the statement 'The circumference of this circle is ' and the circumference afterwards
 
 function calculate (circumference) {
   userInputTwo = document.getElementById('input-two').value
