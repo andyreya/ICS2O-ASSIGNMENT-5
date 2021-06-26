9b5fafbc4656f787d35bf3e7aeeef6cc78825f15
andyreya <83035508+andyreya@users.noreply.github.com>
2021-06-26 04:13:57 +0000

    content, indentation and comments

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-/* creates the variables */
+// creates the variables 
+
 let userInputOne = 0
-let userInputTwo = 0
 let area = 0
-let circumference = 0
 let radius = 0
-let radiusTwo = 0
 
-/* The program will wait for the calculate button to be clicked then it will activate the calculateOne function */
-document.getElementById('button-one' || 'button-two').addEventListener('click', calculate)
+// The program will wait for the calculate button to be clicked then it will activate the calculateOne function 
+document.getElementById('button-one').addEventListener('click', calculate)
 
 
-/* The */
+// the function changes the input into an integer and creates a value for the radius. It calculates the value of the area and rounds it to two decimal places. It then displays the statement 'The area of this circle is ' and the area afterwards
+
 function calculate (area) {
-  if (id = 'one') {
-     userInputOne = document.getElementById('input-one').value 
-    userInputOne = parseInt(userInputOne)
-    radius = userInputOne / 2
-    area = Math.PI * radius ** 2
-    area = area.toFixed(2)
-    document.getElementById('answer-one').innerHTML = ('The area of this circle is ' + area)
-  } else if (id = 'two') {
-    userInputTwo = document.getElementById('input-two').value
-    userInputTwo = parseInt(userInputTwo)
-    radiusTwo = userInputTwo / 2
-    circumference = Math.PI * radius * 2
-    document.getElementById('answer-two').innerHTML = ('The circumference of this circle is ' + circumference)
-  }
+  userInputOne = document.getElementById('input-one').value 
+  userInputOne = parseInt(userInputOne)
+  radius = userInputOne / 2
+  area = Math.PI * radius ** 2
+  area = area.toFixed(2)
+  document.getElementById('answer-one').innerHTML = ('The area of this circle is ' + area)
 }
---
+++ script2.js
@@ -0,0 +1,18 @@
+// creates variables 
+
+let userInputTwo = 0
+let circumference = 0
+
+// makes action occur when button is clicked 
+document.getElementById('button-two').addEventListener('click', calculate)
+
+// the function changes the input into an integer and creates a value for the radius. It calculates the value of the cicumference and rounds it to two decimal places. It then displays the statement 'The circumference of this circle is ' and the cicumference afterwards
+
+function calculate (circumference) {
+  userInputTwo = document.getElementById('input-two').value
+  userInputTwo = parseInt(userInputTwo)
+  radius = userInputTwo / 2
+  circumference = Math.PI * radius * 2
+  circumference = circumference.toFixed(2)
+  document.getElementById('answer-two').innerHTML = ('The circumference of this circle is ' + circumference)
+}
